482c4de5d9b02f6179ff960d5de7bb4dfdba699a
Teemu L <teemulaaks@gmail.com>
2023-11-22 18:13:52 +0200

    change environmental variable

--- src/api.tsx
+++ src/api.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const apiUrl = `${process.env.NEXT_PUBLIC_BACKEND_URL}`;
+export const apiUrl = `${process.env.BACKEND_URL}`;
 
 export async function getTherapists(): Promise<any[]> {
   const res = await axios.get(`${apiUrl}/therapist`);
